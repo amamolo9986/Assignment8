d3c4577e887722b6e2d6eebc838af4b8b505b698
amamolo9986 <amamolo9986@yahoo.com>
2023-07-11 12:53:09 -0500

    first completion

--- src/com/coderscampus/assignment/DataService.java
+++ src/com/coderscampus/assignment/DataService.java
@@ -2,14 +2,16 @@ package com.coderscampus.assignment;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class DataService {
 	
-	public static void fetchData() {
-		
+	public CompletableFuture<List<Integer>> fetchData() {
 		Assignment8 assignment8 = new Assignment8();
 		
 		List<Integer> numbers = new ArrayList<>();
@@ -23,7 +25,13 @@ public class DataService {
 			task.add(tasks);
 		}
 		
-		executor.shutdown();
+		CompletableFuture<Void> allTasks =  CompletableFuture.allOf(task.toArray(new CompletableFuture[0]));
+        return allTasks.thenApplyAsync(v -> numbers);
 	}
-
+	
+	public void countData(List<Integer> numbers) {
+		Map<Integer, Integer> countMap = numbers.stream()
+												.collect(Collectors.toMap(Function.identity(), duplicateValue -> 1, Integer::sum));
+        countMap.forEach((number, count) -> System.out.println(number + "=" + count));
+	}
 }
--- src/com/coderscampus/assignment/MainApplication.java
+++ src/com/coderscampus/assignment/MainApplication.java
@@ -1,9 +1,14 @@
 package com.coderscampus.assignment;
 
-public class MainApplication{
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+
+public class MainApplication {
 
 	public static void main(String[] args) {
-		DataService.fetchData();
+		DataService dataService = new DataService();
+		CompletableFuture<List<Integer>> fetchDataFuture = dataService.fetchData();
+		List<Integer> numbers = fetchDataFuture.join(); // Wait for fetchDataFuture to complete and retrieve the numbers
+		dataService.countData(numbers); // Pass the populated numbers list to countData method
 	}
-
 }
